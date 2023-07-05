54f55a19178f52bd80da824f0089b1c6d9591693
shuchen2000 <49092874+shuchen2000@users.noreply.github.com>
2023-07-05 09:32:09 +0800

    Update test_sta_10seqs.py

--- test_sta_10seqs.py
+++ test_sta_10seqs.py
@@ -9,45 +9,47 @@ import glob
 import os.path as op
 import numpy as np
 import time
-#_woSaf
 
-# 以下需要自定义修改
+########################### configuration ###########################
 
-# 训练完模型sd所在的路径，文件名
+# paths of models' state dicts
 sd_22_path = "./model_state_dicts/STA22_state_dict.pth"
 sd_27_path = "./model_state_dicts/STA27_state_dict.pth"
 sd_32_path = "./model_state_dicts/STA32_state_dict.pth"
 sd_37_path = "./model_state_dicts/STA37_state_dict.pth"
 
-# gt视频 yuv8bits
+# path of ground truth videos (yuv8bits)
 gt_dir = '/home/shuchen/datasets/ScreenContentDataset/RAW/test/8bit'
 
-# lq视频 VTM12.1 yuv10bits
+# paths of VTM12.1 LDP compressed low quality videos with 4 QPs (yuv10bits)
 lq_22_dir = "/home/shuchen/datasets/ScreenContentDataset/LDP/test/VTM_10/22"
 lq_27_dir = "/home/shuchen/datasets/ScreenContentDataset/LDP/test/VTM_10/27"
 lq_32_dir = "/home/shuchen/datasets/ScreenContentDataset/LDP/test/VTM_10/32"
 lq_37_dir = "/home/shuchen/datasets/ScreenContentDataset/LDP/test/VTM_10/37"
-# 测试log文件路
+
+# path of test log file
 log_fp = open("./10seqs_results.log", 'w')
 
+TEST_SEQ_NAME6 = ['WebBro','FSN_12','SlideE','ChinaS','JujuKa','Basket','SlideS','Map_12','HW_128','WordEd']
+TEST_SEQ_SEL = [1,1,1,1,1,1,1,1,1,1]
+
+########################### configuration end ###########################
 
 QPS = [22, 27, 32, 37]
 SD_PATHS = [sd_22_path, sd_27_path, sd_32_path, sd_37_path]
 LQ_DIRS = [lq_22_dir, lq_27_dir, lq_32_dir, lq_37_dir]
 GT_DIR = gt_dir
-TEST_SEQ_NAME6 = ['WebBro','FSN_12','SlideE','ChinaS','JujuKa','Basket','SlideS','Map_12','HW_128','WordEd']
-TEST_SEQ_SEL = [1,1,1,1,1,1,1,1,1,1]
 TEST_SEQ_NAME6_SEL = []
 for index in range(0,10):
     if TEST_SEQ_SEL[index]==1:
         TEST_SEQ_NAME6_SEL.append(TEST_SEQ_NAME6[index])
 
 
-# 以上需要自定义修改
+
 
 
 def test(raw_yuv_path,lq_yuv_path,sd_path,qp):
-    model = Net(frame_channels = 1, mid_channels=64)  # 创建模型
+    model = Net(frame_channels = 1, mid_channels=64)  # Create a STA network
     model.load_state_dict(torch.load(sd_path))
     model = model.cuda()
     model.eval()
@@ -57,7 +59,7 @@ def test(raw_yuv_path,lq_yuv_path,sd_path,qp):
     vname = raw_yuv_path.split("/")[-1].split('.')[0]
     _, wxh, nfs = vname.split('_')
     nfs = int(nfs)
-    w, h = int(wxh.split('x')[0]), int(wxh.split('x')[1])  # 视频各个参数
+    w, h = int(wxh.split('x')[0]), int(wxh.split('x')[1])  # Parameters of video
     print(">>> [" + vname + "]  QP: " + str(qp))
     print("        raw video path: " + raw_yuv_path)
     print("        low quality yuv path: " + lq_yuv_path)
@@ -71,8 +73,8 @@ def test(raw_yuv_path,lq_yuv_path,sd_path,qp):
     lq_y = utils.import_yuv(
         seq_path=lq_yuv_path, h=h, w=w, tot_frm=nfs, start_frm=0, only_y=True, only_u=False, only_v=False, date_type=np.uint16
     )
-    lq_y = lq_y.astype(np.float32) / 4 / 255.  # VTM专用
-    print("            yuv load complete")  # 加载yuv文件为np.array
+    lq_y = lq_y.astype(np.float32) / 4 / 255.
+    print("            yuv load complete")
 
     ori_psnr_counter = utils.Counter()
     enh_psnr_counter = utils.Counter()
@@ -91,10 +93,11 @@ def test(raw_yuv_path,lq_yuv_path,sd_path,qp):
     pbar.set_description(
         "            Enhancing"
     )
-    # 开始增强
-    for index in range(0, nfs):  # 对每一帧进行增强
+    
+    # Start enhance
+    for index in range(0, nfs):  # for each frame
         torch.cuda.empty_cache()
-        # 取5帧，支持帧越界则重复取中间帧
+        # Take 5 adjacent frames as input, and if it exceeds the boundary, repeat the outermost frame to make up for 5 frames
         indexes = [index - 2, index - 1, index, index + 1, index + 2]
         for i in range(0, 5):
             if indexes[i] < 0:
@@ -107,19 +110,19 @@ def test(raw_yuv_path,lq_yuv_path,sd_path,qp):
         f1 = lq_y[:, indexes[3], :, :].unsqueeze(1)
         f2 = lq_y[:, indexes[4], :, :].unsqueeze(1)
 
-        # 增强
+        # Enhance 1 frame with the help of 5 frames
         with torch.no_grad():
             f0_enhanced = model(f0=f0.cuda(), f1=f1.cuda(), f1n=f1n.cuda(), f2=f2.cuda(), f2n=f2n.cuda())
-        # 消除异常值
         f0_enhanced[f0_enhanced > 1] = 1
         f0_enhanced[f0_enhanced < 0] = 0
-
-        # 写入
         enhanced[0, index, 0, :, :] = f0_enhanced
 
         pbar.update()
+        
     pbar.close()
+    
     enhanced = np.float32(enhanced.cpu())
+    
     print("            enhance complete")
     lq_y = np.float32(lq_y.cpu())
     print("    >>> start calculating PSNR and SSIM")
@@ -164,17 +167,17 @@ def main():
         qp = QPS[i]
         sd_path = SD_PATHS[i]
         gt_dir = GT_DIR
-        lq_dir = LQ_DIRS[i]  # 获取路径等信息
+        lq_dir = LQ_DIRS[i]
         print("Start testing qp " + str(qp) + " model on class F")
         print("Start loading model from [" + sd_path + "]")
         msg = "loaded model from [" + sd_path + "]"
         log_fp.write(msg + '\n')
         log_fp.flush()
         
-        gt_video_list = sorted(glob.glob(op.join(gt_dir, '*.yuv')))  # yuv路径列表(gt)
+        gt_video_list = sorted(glob.glob(op.join(gt_dir, '*.yuv')))
 
         for xxxx in range(len(gt_video_list)):
-            raw_yuv_path = gt_video_list[xxxx]  # lq路径以gt路径为准
+            raw_yuv_path = gt_video_list[xxxx]
             lq_yuv_path = lq_dir + "/" + raw_yuv_path.split("/")[len(raw_yuv_path.split("/")) - 1]
             name6 = raw_yuv_path.split("/")[len(raw_yuv_path.split("/")) - 1][0:6]
             if name6 not in TEST_SEQ_NAME6_SEL:
